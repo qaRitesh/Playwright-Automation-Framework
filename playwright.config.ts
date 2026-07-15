bd638affb4735752d0ed666d473b82148c7fc4e7
qaRitesh <myadav7984@gmail.com>
2026-07-15 15:38:59 +0530

    Run Playwright in headless mode on GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    headless: false,
+    headless: process.env.CI ? true : false,
     screenshot:'only-on-failure',
     video:'retain-on-failure',
   },
